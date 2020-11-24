4093adae368923eeb033c8d4ef30e139b5b43bf6
Trevyn <trevyncase@gmail.com>
2020-11-24 17:25:16 -0600

    further changes to support heroku usage

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from models import db, connect_db, User, Post, Tag, PostTag, DEFAULT_IMAGE
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
